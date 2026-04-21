4ecf52a021ccbff99a7cd4effeb5e2c8e3c92e73
Mike <mmrrff2099@gmail.com>
2026-04-21 13:07:22 +0300

    Add static method isMyArray and function MyArrayProto

--- src/index.js
+++ src/index.js
@@ -6,69 +6,80 @@ function MyArray(...args) {
     }
 }
 
-MyArray.prototype[Symbol.iterator] = function* () {
-    for(let i = 0; i < this.length; i++) {
-        yield this[i];
-    }
+MyArray.isMyArray = function (obj) {
+    return obj instanceof MyArray;
+};
+
+function MyArrayProto() {
+    this[Symbol.iterator] = function* () {
+        for (let i = 0; i < this.length; i++) {
+            yield this[i];
+        }
+    };
+
+    this.push = function (...args) {
+        if (args.length !== 0) {
+            for (let i = 0; i < args.length; i++) {
+                this[this.length] = args[i];
+                this.length++;
+            }
+        }
+        return this.length;
+    };
+
+    this.slice = function (start = 0, end = this.length) {
+        let result = new MyArray();
+        for (let i = start; i < end; i++) {
+            result.push(this[i]);
+        }
+        return result;
+    };
+
+    this.reduce = function (callbackfn, initialValue) {
+        let result;
+        let startIndex;
+        let nextIndex;
+
+        if (initialValue !== undefined) {
+            result = initialValue;
+            endIndex = this.length;
+            nextIndex = 0;
+        } else {
+            if (this.length === 0) {
+                return "Reduce of empty array with no initial value";
+            }
+            result = this[0];
+            endIndex = this.length - 1;
+            nextIndex = 1;
+        }
+
+        for (let i = 0; i < endIndex; i++) {
+            result = callbackfn(result, this[i + nextIndex], i, this);
+        }
+
+        return result;
+    };
+
+    this.flat = function (depth = 1) {
+        if (depth <= 0) {
+            return this.slice();
+        }
+
+        return this.reduce((accumulator, currentValue) => {
+            if (MyArray.isMyArray(currentValue) && depth > 0) {
+                accumulator.push(...currentValue.flat(depth - 1));
+            } else {
+                accumulator.push(currentValue);
+            }
+            return accumulator;
+        }, new MyArray());
+    };
 }
 
-MyArray.prototype.push = function (...items) {
-    for (let i = 0; i < items.length; i++) {
-        this[this.length] = items[i];
-        this.length++;
-    }
-    return this.length;
-};
+MyArray.prototype = new MyArrayProto();
 
-MyArray.prototype.slice = function (start = 0, end = this.length) {
-    let result = new MyArray();
-    for (let i = start; i < end; i++) {
-        result.push(this[i]);
-    }
-    return result;
-};
+const myArr = new MyArray(10, 20, new MyArray(30, new MyArray(40, new MyArray(50))));
+console.log(myArr);
 
-MyArray.prototype.reduce = function (callbackfn, initialValue) {
-    let result;
-    let startIndex;
-    let nextIndex;
-
-    if (initialValue !== undefined) {
-        result = initialValue;
-        endIndex = this.length;
-        nextIndex = 0;
-    } else {
-        if (this.length === 0) {
-            return "Reduce of empty array with no initial value";
-        }
-        result = this[0];
-        endIndex = this.length - 1;
-        nextIndex = 1
-    }
-
-    for(let i = 0; i < endIndex; i++) {
-        result = callbackfn(result, this[i + nextIndex], i, this);
-    }
-
-    return result;
-};
-
-MyArray.prototype.flat = function (depth = 1) {
-    if (depth <= 0) {
-        return this.slice();
-    }
-
-    return this.reduce((accumulator, currentValue) => {
-        if (currentValue instanceof MyArray && depth > 0) {
-            accumulator.push(...currentValue.flat(depth - 1));
-        } else {
-            accumulator.push(currentValue);
-        }
-        return accumulator;
-    }, new MyArray());
-};
-
-const myarr = new MyArray(10, 20, new MyArray(30, new MyArray(40, new MyArray(50))));
-console.log(myarr)
-const resultFlat = myarr.flat(1);
+const resultFlat = myArr.flat(2);
 console.log(resultFlat);
